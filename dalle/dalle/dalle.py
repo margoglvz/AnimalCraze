ecfeb78bfccf6de056b35c6720fa58929949265c
Citralyn <alexaduffy1414@gmail.com>
2024-04-20 19:15:28 -0700

    new page

--- dalle/dalle/dalle.py
+++ dalle/dalle/dalle.py
@@ -1,6 +1,7 @@
 """Welcome to Reflex! This file outlines the steps to create a basic app."""
 
 import reflex as rx
+from .newpage import newpage
 from dotenv import load_dotenv
 import os
 
@@ -24,61 +25,49 @@ class State(rx.State):
 
     image_url = ""
     story = ""
-    image_processing = False
-    image_made = False
-    story_processing = False
-    story_made = False
 
-    def get_dalle_result(self, form_data: dict[str, str]):
-        animal = form_data["animal"]
-        name = form_data["name"]
-        emotion = form_data["emotion"]
-        location = form_data["location"]
-        dalle_prompt_text: str = f"A {emotion} {animal} that lives in {location}"
-        chatgtp_prompt_text: str = f"Tell a story in 4 sentences about a {emotion} {animal} \
-            named {name} who lives in {location}" 
-
-        self.image_made = False
-        self.image_processing = True
-        # Yield here so the image_processing take effects and the circular progress is shown.
-        
-        yield
+    def get_image(self, dalle_prompt: str):
         try:
             response = get_openai_client().images.generate(
-                prompt=dalle_prompt_text, n=1, size="1024x1024"
+                prompt=dalle_prompt, n=1, size="1024x1024"
             )
-            self.image_url = response.data[0].url
-            self.image_processing = False
-            self.image_made = True
-            yield
+            image_url = response.data[0].url
+
         except Exception as ex:
-            self.image_processing = False
-            yield rx.window_alert(f"Error with OpenAI Execution. {ex}")
+            return rx.window_alert(f"Error with OpenAI Execution. {ex}")
         
-        self.story_made = False
-        self.story_processing = True
-        
-        yield
+        self.image_url = image_url
+    
+    
+    def get_story(self, chatgtp_prompt: str):
         try:
             response = get_openai_client().chat.completions.create(
                 model="gpt-3.5-turbo",
                 messages=[
-                    {"role": "system", "content": chatgtp_prompt_text}
+                    {"role": "system", "content": chatgtp_prompt}
                 ]
             )
-            self.story = response.choices[0].message.content
-            print(self.story)
-            rx.box(rx.text(self.story,
-                        font_weight="bold",
-                        font_size="2em"))
+            story = response.choices[0].message.content
 
-            self.story_processing = False
-            self.story_made = True
-            yield
         except Exception as ex:
-            self.story_processing = False
-            yield rx.window_alert(f"Error with OpenAI Execution. {ex}")
+            return rx.window_alert(f"Error with OpenAI Execution. {ex}")
 
+        self.story = story
+
+
+    def process(self, form_data: dict[str, str]):
+        animal = form_data["animal"]
+        name = form_data["name"]
+        emotion = form_data["emotion"]
+        location = form_data["location"]
+        dalle_prompt_text = f"A {emotion} {animal} that lives in {location}"
+        chatgtp_prompt_text = f"Tell a story in 4 sentences about a {emotion} {animal} \
+            named {name} who lives in {location}" 
+        
+        self.get_image(dalle_prompt_text)
+        self.get_story(chatgtp_prompt_text)
+
+        return rx.redirect("/newpage")
 
 
 def index():
@@ -116,19 +105,8 @@ def index():
                     spacing="2",
                 ),
                 width="100%",
-                on_submit=State.get_dalle_result,
-            ),
-            rx.divider(),
-            rx.cond(
-                State.image_processing,
-                rx.chakra.circular_progress(is_indeterminate=True),
-                rx.cond(
-                    State.image_made,
-                    rx.image(
-                        src=State.image_url,
-                    ),
-                ),
-            ),
+                on_submit=State.process,
+            ), 
             width="25em",
             bg="white",
             padding="2em",
@@ -139,7 +117,8 @@ def index():
         background="radial-gradient(circle at 22% 11%,rgba(62, 180, 137,.20),hsla(0,0%,100%,0) 19%),radial-gradient(circle at 82% 25%,rgba(33,150,243,.18),hsla(0,0%,100%,0) 35%),radial-gradient(circle at 25% 61%,rgba(250, 128, 114, .28),hsla(0,0%,100%,0) 55%)",
     )
 
-
+def new():
+    return newpage(State)
 
 # Add state and page to the app.
 app = rx.App(
@@ -148,6 +127,6 @@ app = rx.App(
     ),
 )
 
-
 app.add_page(index, title="Reflex:DALL-E")
+app.add_page(newpage(State), route="newpage", title="REFLEX:NEW")
 
---
+++ dalle/dalle/newpage.py
@@ -0,0 +1,30 @@
+import reflex as rx
+
+
+def newpage(State):
+
+    """New Page"""
+    image_url = State.image_url
+    story = State.story
+    
+    return rx.center(
+        rx.vstack(
+            rx.heading("Results"),
+            rx.image(src=image_url, width="100px", height="auto"),
+            rx.divider(),
+            rx.box(rx.text(story,
+            font_weight="bold",
+            font_size="2em")),
+            bg="white",
+            padding_x="5em",
+            padding_y="2em",
+            border_radius="25px",
+            align_items="left",
+            overflow="auto",
+        ),
+        padding="1em",
+        height="100vh",
+        align_items="top",
+        bg="#ededed",
+        overflow="auto",
+    )
