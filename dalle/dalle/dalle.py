1d2ba54150f6b9abb3802bcb002ec16d378f75b3
Citralyn <alexaduffy1414@gmail.com>
2024-04-20 19:31:44 -0700

    re-edit of dalle

--- dalle/dalle/dalle.py
+++ dalle/dalle/dalle.py
@@ -4,7 +4,6 @@ import reflex as rx
 from .newpage import newpage
 from dotenv import load_dotenv
 import os
-from .AnimalResults import AnimalResults
 
 import openai
 
@@ -20,42 +19,21 @@ def get_openai_client():
 
     return _client
 
-
 class State(rx.State):
     """The app state."""
 
     image_url = ""
     story = ""
-    image_processing = False
-    image_made = False
-    story_processing = False
-    story_made = False
 
-    def get_dalle_result(self, form_data: dict[str, str]):
-        animal = form_data["animal"]
-        name = form_data["name"]
-        emotion = form_data["emotion"]
-        location = form_data["location"]
-        dalle_prompt_text: str = f"A {emotion} {animal} that lives in {location}"
-        chatgtp_prompt_text: str = f"Tell a story in 4 sentences about a {emotion} {animal} \
-            named {name} who lives in {location}" 
-
-        self.image_made = False
-        self.image_processing = True
-        # Yield here so the image_processing take effects and the circular progress is shown.
-        
-        yield
+    def get_image(self, dalle_prompt: str):
         try:
             response = get_openai_client().images.generate(
-                prompt=dalle_prompt_text, n=1, size="1024x1024"
+                prompt=dalle_prompt, n=1, size="1024x1024"
             )
-            self.image_url = response.data[0].url
-            self.image_processing = False
-            self.image_made = True
-            yield
+            image_url = response.data[0].url
+
         except Exception as ex:
-            self.image_processing = False
-            yield rx.window_alert(f"Error with OpenAI Execution. {ex}")
+            return rx.window_alert(f"Error with OpenAI Execution. {ex}")
         
         self.image_url = image_url
     
@@ -68,19 +46,27 @@ class State(rx.State):
                     {"role": "system", "content": chatgtp_prompt}
                 ]
             )
-            self.story = response.choices[0].message.content
-            print(self.story)
-            rx.box(rx.text(self.story,
-                        font_weight="bold",
-                        font_size="2em"))
+            story = response.choices[0].message.content
 
-            self.story_processing = False
-            self.story_made = True
-            yield
         except Exception as ex:
-            self.story_processing = False
-            yield rx.window_alert(f"Error with OpenAI Execution. {ex}")
+            return rx.window_alert(f"Error with OpenAI Execution. {ex}")
 
+        self.story = story
+
+
+    def process(self, form_data: dict[str, str]):
+        animal = form_data["animal"]
+        name = form_data["name"]
+        emotion = form_data["emotion"]
+        location = form_data["location"]
+        dalle_prompt_text = f"A {emotion} {animal} that lives in {location}"
+        chatgtp_prompt_text = f"Tell a story in 4 sentences about a {emotion} {animal} \
+            named {name} who lives in {location}" 
+        
+        self.get_image(dalle_prompt_text)
+        self.get_story(chatgtp_prompt_text)
+
+        return rx.redirect("/newpage")
 
 
 def index():
@@ -91,28 +77,27 @@ def index():
                 rx.vstack(
                     rx.input(
                         id="animal",
-                        placeholder="Enter an animal",
+                        placeholder="Enter a prompt..",
                         size="3",
                     ),
                     rx.input(
                         id="name",
-                        placeholder="Enter a name",
+                        placeholder="Enter a prompt..",
                         size="3",
                     ),
                     rx.input(
                         id="emotion",
-                        placeholder="Enter an emotion",
+                        placeholder="Enter a prompt..",
                         size="3",
                     ),
                     rx.input(
                         id="location",
-                        placeholder="Enter a location",
+                        placeholder="Enter a prompt..",
                         size="3",
                     ),
                     rx.button(
                         "SUBMIT",
-                        on_click=rx.redirect("C:\\Users\\msgal\\Developer\\AnimalCraze\\dalle\\dalle\\AnimalResults.py"),
-                        external=True,
+                        type="submit",
                         size="3",
                     ),
                     align="stretch",
@@ -128,8 +113,7 @@ def index():
         ),
         width="100%",
         height="100vh",
-
-        background= "radial-gradient(circle at 22% 11%, rgba(41, 242, 194, .20), hsla(0,0%,100%,0) 19%), radial-gradient(circle at 82% 25%, rgba(19, 128, 255, .25), hsla(0,0%,100%,0) 55%), radial-gradient(circle at 25% 61%, rgba(185, 77, 251, .28), hsla(0,0%,100%,0) 55%)"
+        background="radial-gradient(circle at 22% 11%,rgba(62, 180, 137,.20),hsla(0,0%,100%,0) 19%),radial-gradient(circle at 82% 25%,rgba(33,150,243,.18),hsla(0,0%,100%,0) 35%),radial-gradient(circle at 25% 61%,rgba(250, 128, 114, .28),hsla(0,0%,100%,0) 55%)",
     )
 
 def new():
@@ -142,6 +126,6 @@ app = rx.App(
     ),
 )
 
-app.add_page(index, title="Reflex:DALL-E")
 
-#rx.redirect
+app.add_page(index, title="Reflex:DALL-E")
+app.add_page(newpage(State), route="newpage", title="REFLEX:NEW")
