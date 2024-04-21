16ae39b4fa293546af187ecba1617cd680adf445
margoglvz <msgalvez06@gmail.com>
2024-04-21 02:13:30 -0700

    padding + css changes

--- dalle/dalle/newpage.py
+++ dalle/dalle/newpage.py
@@ -23,6 +23,10 @@ def newpage(State):
                 border_radius="25px",
                 align_items="left",
                 overflow="auto",
+                width="80%",
+                margin="auto",
+                margin_top="10em",
+                margin_bottom="1em"
             ),
             rx.vstack(
                 rx.heading(f"Facts about {(State.animal).lower()}s!"),
@@ -36,11 +40,15 @@ def newpage(State):
                 border_radius="25px",
                 align_items="left",
                 overflow="auto",
+                width="80%",
+                margin="auto",
+                # margin_top="10em",
+                margin_bottom="2em"
             ),
         ),
         padding="1em",
         height="100vh",
         align_items="top",
-        bg="#1B5E3A",
+        background="radial-gradient(circle at 22% 11%, rgba(27, 94, 58, .20), hsla(0, 0%, 100%, 0) 80%), radial-gradient(circle at 82% 25%, rgba(41, 242, 194, .98), hsla(0, 0%, 100%, 0) 55%), radial-gradient(circle at 25% 61%, rgba(19, 128, 255, .8), hsla(0, 0%, 100%, 0) 105%)",
         overflow="auto",
     )
